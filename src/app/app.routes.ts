5f9070d30d3f61db9e379c6102940b5ea171725d
Draiphis <val08350@gmail.com>
2026-05-11 09:39:16 +0200

    feat (connexion) : création de la route vers la page connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Connexion } from './pages/connexion/connexion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'connexion',
+    component: Connexion,
+  },
+];
