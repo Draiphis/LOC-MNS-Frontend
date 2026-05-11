0b92b8e11cba9b1d50e397b989de4e38c5d95cc6
Draiphis <val08350@gmail.com>
2026-05-11 10:43:37 +0200

    feat (catalogue) : ajout de la route du catalogue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Connexion } from './pages/connexion/connexion';
+import { Catalogue } from './pages/catalogue/catalogue';
 
 export const routes: Routes = [
   {
     path: 'connexion',
     component: Connexion,
+  },
+  {
+    path: 'catalogue',
+    component: Catalogue,
   },
 ];
