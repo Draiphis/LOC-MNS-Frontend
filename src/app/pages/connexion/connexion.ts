5258ba0f835208cd215d08f562cab0c3ff2f1bd8
Draiphis <val08350@gmail.com>
2026-05-19 11:04:23 +0200

    feat (connexion) : ajout de la page de connexion dynamique

--- src/app/pages/connexion/connexion.ts
+++ src/app/pages/connexion/connexion.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-connexion',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './connexion.html',
   styleUrl: './connexion.css',
 })
-export class Connexion {}
+export class Connexion {
+  formBuilder = inject(FormBuilder);
+  httpClient = inject(HttpClient);
+  route = inject(ActivatedRoute);
+
+  formulaire = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    motDePasse: ['', [Validators.required]],
+  });
+
+  onConnexion() {
+    if (this.formulaire.valid) {
+      this.httpClient.post('http://localhost:8080/connexion', this.formulaire.value, {
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (jwt)=>{
+          localStorage.setItem('jwt', jwt);
+          this.notification.open('Connexion réussie, 'valid')
+          }, error : (err)=> {
+            this.notification.open('Mauvais login/mot de passe', 'error')
+          }
+        }
+      });
+    }
+  }
+}
