75e2eedb071874e8717547f81e951beaf71c85f8
Draiphis <val08350@gmail.com>
2026-05-19 12:03:26 +0200

    feat (guard) : ajout du guard : impossible de voir les pages si l'utilisateur n'a pas rentré de jwt. le contenu dynamique ne s'affiche pas si le jwt est mauvais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Connexion } from './pages/connexion/connexion';
 import { Catalogue } from './pages/catalogue/catalogue';
 import { DemandeEmprunt } from './pages/demande-emprunt/demande-emprunt';
+import { utilisateurGuard } from './guards/utilisateur-guard';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {
     path: 'catalogue',
     component: Catalogue,
+    canActivate: [utilisateurGuard],
   },
   {
     path: 'emprunt',
--- src/app/guards/utilisateur-guard.ts
+++ src/app/guards/utilisateur-guard.ts
@@ -1,5 +1,11 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
 
 export const utilisateurGuard: CanActivateFn = (route, state) => {
+  if (localStorage.getItem('jwt') == null) {
+    const router = inject(Router);
+    return router.parseUrl('/connexion');
+  }
+
   return true;
 };
