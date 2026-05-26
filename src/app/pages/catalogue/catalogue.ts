728f820ff6a7f2dcfa2ab0e6644461bf59d13160
Draiphis <val08350@gmail.com>
2026-05-26 14:53:40 +0200

    feat (catalogue) : ajout de la possibilité de filtrer uniquement les modèles disponibles

--- src/app/pages/catalogue/catalogue.ts
+++ src/app/pages/catalogue/catalogue.ts
@@ -10,15 +10,38 @@ import { RouterModule } from '@angular/router';
 })
 export class Catalogue {
   modeles = signal<Modele[]>([]);
+  types = signal<Type[]>([]);
+
+  estDisponibleImmediatement = signal(false);
 
   httpClient = inject(HttpClient);
 
   ngOnInit() {
-    this.httpClient.get<Modele[]>('http://localhost:8080/modele/list').subscribe((listModeles) => {
-      console.log(listModeles);
+    this.chargerModeles();
+    this.chargerTypes();
+    console.log('fin');
+  }
+
+  filtreDisponibleMaintenant(event: Event) {
+    const verifier = (event.target as HTMLInputElement).checked;
+    this.estDisponibleImmediatement.set(verifier);
+
+    this.chargerModeles();
+  }
+
+  chargerModeles() {
+    const url = this.estDisponibleImmediatement()
+      ? 'http://localhost:8080/modele/list-filtrer'
+      : 'http://localhost:8080/modele/list';
+
+    this.httpClient.get<Modele[]>(url).subscribe((listModeles) => {
       this.modeles.set(listModeles);
     });
+  }
 
-    console.log('fin');
+  chargerTypes() {
+    this.httpClient.get<Type[]>('http://localhost:8080/type/list').subscribe((listTypes) => {
+      this.types.set(listTypes);
+    });
   }
 }
