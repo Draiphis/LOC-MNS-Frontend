c75f75e970369631977f8048655a245fcf530503
Draiphis <val08350@gmail.com>
2026-06-19 16:28:26 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cda-valentin.stagiairesmns.fr',
+  apiUrl: '/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cda-valentin.stagiairesmns.fr',
+  apiUrl: '/api',
 };
